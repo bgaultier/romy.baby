0798702096ad3fabcdeb4cc42939611c9a745513
Baptiste Gaultier <baptiste@laboite.cc>
2018-07-25 10:56:39 +0200

    arch fixed

--- romy/romy/settings.py
+++ romy/romy/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'romy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'romy/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "romy", "static"),
+]
