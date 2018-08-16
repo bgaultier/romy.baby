5a662b197ce090aaa905d96a360975cbf251306f
Baptiste Gaultier <baptiste@laboite.cc>
2018-08-16 17:37:22 +0200

    send_mail bug fixed

--- romy/activities/models.py
+++ romy/activities/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
+from django.core.mail import send_mail
 
 import uuid
 
